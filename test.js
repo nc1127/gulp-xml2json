016cbba0da99ac8e094bc5889fbcf6d848869235
howardchi <howardchi@fougood.com>
2014-02-17 18:21:51 +0800

    add test

--- test.js
+++ test.js
@@ -2,17 +2,18 @@
 var fs = require('fs');
 var assert = require('assert');
 var gutil = require('gulp-util');
-var cssmin = require('./index');
+var xml2json = require('./index');
 
-it('should minify css', function (cb) {
-	var stream = cssmin();
+it('should convert xml to json', function (cb) {
+	var stream = xml2json();
 	stream.on('data', function (file) {
-		assert();
+    var parse_str = JSON.parse(file.contents.toString('utf-8'));
+		assert(parse_str instanceof Object);
 		cb();
 	});
 
 	stream.write(new gutil.File({
-		path: __dirname + '/sample/test.css',
-		contents: fs.readFileSync(__dirname + '/sample/test.css')
+		path: __dirname + '/sample/A_lvr_land_A.xml',
+		contents: fs.readFileSync(__dirname + '/sample/A_lvr_land_A.xml')
 	}));
 });
